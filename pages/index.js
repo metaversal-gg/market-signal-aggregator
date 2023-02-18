1bfea623b5fb0f64b502167bbd091a175948f272
ZILECAO <zilecao@sas.upenn.edu>
2023-02-18 11:59:08 -0500

    fixed text styling, removed package-lock

--- pages/index.js
+++ pages/index.js
@@ -40,14 +40,12 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>#Web3Wednesday Tweet Generator</h1>
+            <h1>#Web3 Tweet Thread Generator</h1>
           </div>
           <div className="header-subtitle">
             <h2>what do you want to post about? (max output: 1000 characters)</h2>
-
-          </div>
-          <div className="header-subtitle2">
-            <h2>see inspiration from the <a href="https://twitter.com/weavechainweb3">weavechain twitter</a></h2>
+            <h4>sometimes the app will time-out due to vercel run-time limiter!</h4>
+            <h4>see inspiration from the <a href="https://twitter.com/weavechainweb3">weavechain twitter</a> 🙂</h4>
           </div>
         </div>
         <div className="prompt-container">
